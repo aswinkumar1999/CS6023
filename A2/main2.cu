642ff2f21c592287deffe0c3fa8796b272594d10
Aswinkumar <aswinkumar1999@gmail.com>
2020-03-01 23:35:13 +0530

    Done

--- A2/main2.cu
+++ A2/main2.cu
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #define min(X, Y) (((X) < (Y)) ? (X) : (Y))
 
-
+// Kernel Function to Find Sum
 __global__ void sumRandC (int* A, int* B, int m, int n, int k=1){
+    // Get Id
     unsigned long long int id = ( blockIdx.x*blockDim.y+threadIdx.y)*blockDim.x + threadIdx.x;
-    // printf("%d\n",id);
+    // For Selected threads
     if(id<(m*n)/k){
-      // printf("True");
       for (int i=k*id;i<(k)*(id+1);i++){
-        // printf("%d\t",i);
         int n_new = i %(n);
         int m_new = i /(n);
-        // printf("%d\t",m_new);
-        // printf("%d\t",n_new);
         // Add values
         atomicAdd(&B[m_new*(n+1)+n], B[m_new*(n+1)+n_new]);
         atomicAdd(&B[m*(n+1)+n_new],  B[m_new*(n+1)+n_new]);
       }
     }
 }
+
+// Define Global Variable - Mini
 __device__ int mini;
+// Function to Find the Minimum value
 __global__ void findMin (int* A, int* B, int m, int n, int k=1){
+  // Create Thread ID
   unsigned long long int id = ( blockIdx.x*blockDim.y+threadIdx.y)*blockDim.x + threadIdx.x;
+  // Initialise Mini with one element of first row
   if(id==0){
     mini=B[n];
   }
+  // For Selected Threads we do Atomic Operation to compute min.
   if(id<(m+n)){
     if(id < m ){
       atomicMin(&mini,B[id*(n+1)+n]);
@@ -32,14 +35,17 @@ __global__ void findMin (int* A, int* B, int m, int n, int k=1){
     if(id>=m){
       atomicMin(&mini,B[m*n+id]);
     }
+  }
 }
+// Kernel Function to Add minimum and update minimum
+__global__ void updateMin (int* A, int* B, int m, int n, int k=1){
+  // Get Thread ID
+  unsigned long long int id = ( blockIdx.x*blockDim.y+threadIdx.y)*blockDim.x + threadIdx.x;
+  // Set Last Element from Calculated Minimum
   if(id==0){
     B[m*(n+1)+n]=mini;
   }
-}
-
-__global__ void updameMin (int* A, int* B, int m, int n, int k=1){
-  unsigned long long int id = ( blockIdx.x*blockDim.y+threadIdx.y)*blockDim.x + threadIdx.x;
+  // Add Minimum to everything
   if(id<(m*n)/k){
     for (int i=k*id;i<(k)*(id+1);i++){
       int n_new = i %(n);
@@ -49,6 +55,7 @@ __global__ void updameMin (int* A, int* B, int m, int n, int k=1){
   }
 }
 
+// Helper Function to Print Matrix
 void print_matrix(int* mat,int m, int n) {
     for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++) {
@@ -57,9 +64,9 @@ void print_matrix(int* mat,int m, int n) {
         printf("\n");
     }
 }
-
+// CPU Computing Function
 void cpu(int *in,int m,int n){
-  //Row
+  //Row Computation
   int i,j,sum=0;
   for(i=0; i<m; i++) {
     sum=0;
@@ -68,37 +75,31 @@ void cpu(int *in,int m,int n){
       }
     in[i*(n+1)+n]=sum;
   }
-  //Column
+  //Column Computation
   for(i=0; i<n; i++) {
     sum=0;
       for(j=0; j<m; j++) {
               sum+=in[j*(n+1)+i];
       }
     in[(n+1)*(m)+i]=sum;
-
   }
-  // Find min
+  // Find minimum
   long long int mini=100*max(m,n);
-
   for(i=0;i<n;i++){
-    // printf("%d\t",in[((m)*(n+1))+i] );
     mini = min(mini,in[((m)*(n+1))+i]);
-    // printf("%d\n", mini);
   }
   for(i=0;i<m;i++){
-      // printf("%d\t",in[(i)*(n+1)+n]);
     mini = min(mini,in[(i)*(n+1)+n]);
-    // printf("%d\n", mini);
   }
   in[(n+1)*(m)+n]=mini;
-  // Add Min
+  // Add Minimum values to everything
   for(i=0; i<m; i++) {
       for(j=0; j<n; j++) {
               in[i*(n+1) + j] += mini;
       }
   }
-
 }
+// Helper Function to Check if CPU and GPU Computation are same.
 bool check_same(int *C,int *D,int m,int n){
   for(int i=0; i<m*n; i++) {
         if(C[i]!=D[i]){
@@ -111,6 +112,7 @@ bool check_same(int *C,int *D,int m,int n){
 }
 int main()
 {
+    // Define and Initialize Variables
     long long unsigned M,N,K,i,j;
     scanf("%llu", &M);
     scanf("%llu", &N);
@@ -127,7 +129,7 @@ int main()
     B_cout = (int*)malloc((M+1)*(N+1)*sizeof(int));
     B_gout = (int*)malloc((M+1)*(N+1)*sizeof(int));
 
-    // print_matrix(mathost, N);
+    // Initialize B Matrix to Zero
     for(i=0; i<=M; i++) {
         for(j=0; j<=N; j++) {
                 B_cin[i*(N+1) + j] = 0;
@@ -135,40 +137,30 @@ int main()
         }
     }
     printf("\n");
-    // Initialize lower triangular matrix
+    // Initialize Matrix from Input and Initialize B Matrix
     for(i=0; i<M; i++) {
         for(j=0; j<N; j++) {
-                scanf("%llu", &A_cin[i*N + j]);
+                scanf("%d", &A_cin[i*N + j]);
                 B_cin[i*(N+1)+j] = A_cin[i*N + j];
                 B_cout[i*(N+1)+j] = A_cin[i*N + j];
         }
     }
-
-    cpu(B_cout,M,N);
-
-
+    // CPU Computation
+      // cpu(B_cout,M,N);
+    // Copy Matrices
     cudaMemcpy(A_gin, A_cin, M * N * sizeof(int),cudaMemcpyHostToDevice);
     cudaMemcpy(B_gin, B_cin, (M+1) * (N+1) * sizeof(int),cudaMemcpyHostToDevice);
-
-
-    sumRandC <<< 1000, 32 >>> (A_gin,B_gin,M,N,5);
+    // Define Parameters
+    int blockdim = 1024;
+    int griddim = ceil((float)(M*N/K) / blockdim);
+    // Launch kernels
+    sumRandC <<< griddim, blockdim >>> (A_gin,B_gin,M,N,K);
     cudaDeviceSynchronize();
-    findMin <<< 1000, 32 >>> (A_gin,B_gin,M,N,1);
+    findMin <<< griddim, blockdim >>> (A_gin,B_gin,M,N,K);
     cudaDeviceSynchronize();
-    updameMin <<< 1000, 32 >>> (A_gin,B_gin,M,N,1);
+    updateMin <<< griddim, blockdim >>> (A_gin,B_gin,M,N,K);
     cudaDeviceSynchronize();
     cudaMemcpy(B_gout, B_gin, (M+1) * (N+1) * sizeof(int),cudaMemcpyDeviceToHost);
-
-    // printf("\n");
-    // print_matrix(B_cin,M+1,N+1);
-    // printf("\n");
-    // print_matrix(B_gout,M+1,N+1);
-    // printf("\n");
-    // print_matrix(B_cout,M+1,N+1);
-
-    printf("%d\n", check_same(B_gout,B_cout,M+1,N+1));
-    //
-
-
-    printf("\n");
+    // Print Matrix
+    print_matrix(B_gout,M+1,N+1);
 }
